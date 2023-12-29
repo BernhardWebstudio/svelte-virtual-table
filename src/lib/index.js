1e6c501ad8680dd3b44e08de6279c6aa63bf15b5
Tim Bernhard <tim@bernhard-webstudio.ch>
2023-12-29 09:11:13 +0100

    First implementation starting from svelte-kit starter kit

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,4 @@
+// Reexport your entry components here
 import VirtualTable from './VirtualTable.svelte'
 
 export default VirtualTable
---
+++ svelte.config.js
@@ -0,0 +1,15 @@
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
+};
+
+export default config;
